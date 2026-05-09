e07639c22f957e364450095abc0a4e23c38ca4f2
haruky <haruky.m@proton.me>
2026-05-09 22:06:00 +0100

    FIX: Performance Enhance

--- src/engine/input/InputManager.cpp
+++ src/engine/input/InputManager.cpp
@@ -1,28 +1,38 @@
 #include <engine/input/InputManager.hpp>
 
+#include <algorithm>
+#include <functional>
+
 
 Action InputManager::verifyInput( const Request& request, const Input& input ) {
-    Action out;
+    // [KEYBOARD]============================================
+    std::function<bool( const Request::kbBinding& )> p1
+        = [&]( const Request::kbBinding& B ) {
+        return input.keyb.key == B.key;
+    };
+    const auto& A = std::find_if(
+        request.vitalKeys.begin(),
+        request.vitalKeys.end(),
+        p1
+    );
 
-    // [KEYBOARD]
-    if ( input.keyb.clicked )
-        for ( const auto& binding : request.vitalKeys )
-            if ( binding.key == input.keyb.key ) {
-                // out = binding.act;
-                out = request.getAction( binding.key );
-                break;
-            }
+    if ( A != request.vitalKeys.end() ) {
+        return A->act;
+    } else return Action::None;
 
+    // [MOUSE]==============================================
+    std::function<bool( const Request::msBinding& )> p2
+        = [&]( const Request::msBinding& B ) {
+            return ( input.mouse.btn == B.btn )
+                && ( B.bounds->contains(input.mouse.pos) );
+        };
+    const auto& B = std::find_if(
+        request.vitalButtons.begin(),
+        request.vitalButtons.end(),
+        p2
+    );
 
-    // [MOUSE]
-    if ( input.mouse.clicked )
-        for ( const auto& binding : request.vitalButtons )
-            if (( binding.btn == input.mouse.btn )
-                && ( binding.bounds->contains( input.mouse.pos ))) {
-                    // out = binding.act;
-                    out = request.getAction( binding.btn, *(binding.bounds) );
-                    break;
-                }
-
-    return out;
+    if ( B != request.vitalButtons.end() ) {
+        return B->act;
+    } else return Action::None;
 }

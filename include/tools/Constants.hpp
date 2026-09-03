1c0229cdae524fe524728a6907d54a22d29692ec
haruky <haruky.m@proton.me>
2026-09-03 21:35:17 +0100

    NEW: Constants class into namespace

--- include/tools/Constants.hpp
+++ include/tools/Constants.hpp
@@ -7,33 +7,47 @@
 #include <array>
 
 
-class Constants {
-    public:
-        static int P1_SCORE;
-        static int P2_SCORE;
-        static int CD;
-        static int maxCD;
-        static int maxScore;
-        static int DELAY_t;
-        static int ballAccel;
-        static char ballOrient;
-        static int buffingPeriod;
-        static int freezeDuration;
-        static float offsetPercent;
-        static float stretchPercent;
+namespace Constants {
+    inline Json::Type MODE = []( std::string_view mode ) -> Json::Type {
+        if ( mode=="easy" ) return Json::Type::EASY;
+        if ( mode=="medium" ) return Json::Type::MED;
+        if ( mode=="hard" ) return Json::Type::HARD;
 
-        static int WIDTH;
-        static int HEIGHT;
-        static int W_EDGE;
-        static sf::Vector2f W_CTR;
-        static Json::Type MODE;
+        throw std::runtime_error("Invalid 'mode string' given for 'Constants::_match_mode'");
 
-        enum class Sides {
-            TOP, LEFT, BOTTOM, RIGHT
-        };
+    }(Json::String("setting.mode", Json::Type::SET));
 
-        static std::array< sf::Vector2f, 4 > Norms;
+    inline const int DELAY_t = 200;
 
-    private:
-        static Json::Type _match_mode( const std::string_view );
+    inline const int maxCD =  Json::Int("setting.maxCD", Json::Type::SET);
+    inline const int WIDTH =  Json::Int("win.W", Json::Type::SET);
+    inline const int HEIGHT = Json::Int("win.H", Json::Type::SET);
+    inline const int W_EDGE = Json::Int("win.edge", Json::Type::SET);
+
+    inline const sf::Vector2f W_CTR = {
+        Constants::WIDTH / 2.0f,
+        Constants::HEIGHT / 2.0f
+    };
+
+    inline const  std::array< const sf::Vector2f, 4 > Norms = {
+        sf::Vector2f( {0.0f, -1.0f} ),
+        sf::Vector2f( {-1.0f, 0.0f} ),
+        sf::Vector2f( {0.0f,  1.0f} ),
+        sf::Vector2f( {1.0f,  0.0f} )
+    };
+
+    enum class Sides {
+        TOP, LEFT, BOTTOM, RIGHT
+    };
+
+
+    inline int P1_SCORE = 0;
+    inline int P2_SCORE = 0;
+    inline int CD = 0;
+    inline int maxScore = Json::Int("setting.maxScore", Json::Type::SET);
+    inline char ballOrient = '?';
+    inline int buffingPeriod = Json::Int("setting.buffingPeriod");
+    inline int freezeDuration = Json::Int("buff.freeze.durPivot");
+    inline float offsetPercent = Json::Float("setting.offsetPercent");
+    inline float stretchPercent = Json::Float("setting.stretchPercent");
 };
